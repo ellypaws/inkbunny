5b9dcd815bf85c6cca5c3daa895286e3b03e5cd5
Elly <145195760+ellypaws@users.noreply.github.com>
2024-03-03 21:47:25 +0800

    API returns ints as strings and prices as $USD

--- api/search.go
+++ api/search.go
@@ -8,51 +8,51 @@ import (
 )
 
 type SearchRequest struct {
-	SID                string `json:"sid"`
-	OutputMode         string `json:"output_mode,omitempty"`
-	RID                string `json:"rid,omitempty"`
-	SubmissionIDsOnly  string `json:"submission_ids_only,omitempty"`
-	SubmissionsPerPage int    `json:"submissions_per_page,omitempty"`
-	Page               int    `json:"page,omitempty"`
-	KeywordsList       string `json:"keywords_list,omitempty"`
-	NoSubmissions      string `json:"no_submissions,omitempty"`
-	GetRID             string `json:"get_rid,omitempty"`
-	FieldJoinType      string `json:"field_join_type,omitempty"` // "or" or "and"
-	Text               string `json:"text,omitempty"`
-	StringJoinType     string `json:"string_join_type,omitempty"`
-	Keywords           string `json:"keywords,omitempty"`
-	Title              string `json:"title,omitempty"`
-	Description        string `json:"description,omitempty"`
-	MD5                string `json:"md5,omitempty"`
-	KeywordID          string `json:"keyword_id,omitempty"`
-	Username           string `json:"username,omitempty"`
-	UserID             string `json:"user_id,omitempty"`
-	FavsUserID         string `json:"favs_user_id,omitempty"`
-	UnreadSubmissions  string `json:"unread_submissions,omitempty"`
-	Type               string `json:"type,omitempty"`
-	Sales              string `json:"sales,omitempty"`
-	PoolID             string `json:"pool_id,omitempty"`
-	OrderBy            string `json:"orderby,omitempty"` // Values: create_datetime, unread_datetime, views, total_print_sales, total_digital_sales, total_sales, username, fav_datetime, fav_stars, pool_order. Default: create_datetime.
-	DaysLimit          int    `json:"dayslimit,omitempty"`
-	Random             string `json:"random,omitempty"`
-	Scraps             string `json:"scraps,omitempty"`
-	CountLimit         int    `json:"count_limit,omitempty"`
+	SID                string    `json:"sid"`
+	OutputMode         string    `json:"output_mode,omitempty"`
+	RID                string    `json:"rid,omitempty"`
+	SubmissionIDsOnly  string    `json:"submission_ids_only,omitempty"`
+	SubmissionsPerPage IntString `json:"submissions_per_page,omitempty"`
+	Page               IntString `json:"page,omitempty"`
+	KeywordsList       string    `json:"keywords_list,omitempty"`
+	NoSubmissions      string    `json:"no_submissions,omitempty"`
+	GetRID             string    `json:"get_rid,omitempty"`
+	FieldJoinType      string    `json:"field_join_type,omitempty"` // "or" or "and"
+	Text               string    `json:"text,omitempty"`
+	StringJoinType     string    `json:"string_join_type,omitempty"`
+	Keywords           string    `json:"keywords,omitempty"`
+	Title              string    `json:"title,omitempty"`
+	Description        string    `json:"description,omitempty"`
+	MD5                string    `json:"md5,omitempty"`
+	KeywordID          string    `json:"keyword_id,omitempty"`
+	Username           string    `json:"username,omitempty"`
+	UserID             string    `json:"user_id,omitempty"`
+	FavsUserID         string    `json:"favs_user_id,omitempty"`
+	UnreadSubmissions  string    `json:"unread_submissions,omitempty"`
+	Type               string    `json:"type,omitempty"`
+	Sales              string    `json:"sales,omitempty"`
+	PoolID             string    `json:"pool_id,omitempty"`
+	OrderBy            string    `json:"orderby,omitempty"` // Values: create_datetime, unread_datetime, views, total_print_sales, total_digital_sales, total_sales, username, fav_datetime, fav_stars, pool_order. Default: create_datetime.
+	DaysLimit          IntString `json:"dayslimit,omitempty"`
+	Random             string    `json:"random,omitempty"`
+	Scraps             string    `json:"scraps,omitempty"`
+	CountLimit         IntString `json:"count_limit,omitempty"`
 }
 
 type SearchResponse struct {
 	Sid                  string            `json:"sid"`
 	UserLocation         string            `json:"user_location"`
-	ResultsCountAll      int               `json:"results_count_all"`
-	ResultsCountThisPage int               `json:"results_count_thispage"`
-	PagesCount           int               `json:"pages_count"`
-	Page                 int               `json:"page"`
+	ResultsCountAll      IntString         `json:"results_count_all"`
+	ResultsCountThisPage IntString         `json:"results_count_thispage"`
+	PagesCount           IntString         `json:"pages_count"`
+	Page                 IntString         `json:"page"`
 	RID                  string            `json:"rid,omitempty"`
 	RIDTTL               string            `json:"rid_ttl,omitempty"`
 	SearchParams         map[string]string `json:"search_params"`
 	KeywordList          []struct {
-		KeywordID        string `json:"keyword_id"`
-		KeywordName      string `json:"keyword_name"`
-		SubmissionsCount int    `json:"submissions_count"`
+		KeywordID        string    `json:"keyword_id"`
+		KeywordName      string    `json:"keyword_name"`
+		SubmissionsCount IntString `json:"submissions_count"`
 	} `json:"keyword_list,omitempty"`
 	Submissions []struct {
 		SubmissionBasic
--- api/submissions.go
+++ api/submissions.go
@@ -7,42 +7,6 @@ import (
 	"strings"
 )
 
-// BooleanYN is a custom type to handle boolean values marshaled as "yes" or "no".
-type BooleanYN bool
-
-const (
-	Yes BooleanYN = true
-	No  BooleanYN = false
-
-	True  BooleanYN = true
-	False BooleanYN = false
-)
-
-// MarshalJSON converts the BooleanYN boolean into a JSON string of "yes" or "no".
-func (b BooleanYN) MarshalJSON() ([]byte, error) {
-	if b {
-		return json.Marshal("yes")
-	}
-	return json.Marshal("no")
-}
-
-// UnmarshalJSON parses a JSON "yes" or "no" into a BooleanYN boolean.
-func (b *BooleanYN) UnmarshalJSON(data []byte) error {
-	var s string
-	if err := json.Unmarshal(data, &s); err != nil {
-		return err
-	}
-	switch s {
-	case "t", "yes", "true":
-		*b = true
-	case "f", "no", "false":
-		*b = false
-	default:
-		return fmt.Errorf(`allowed values for Boolean ["t","yes","true","f","no","false"], got "%s"`, s)
-	}
-	return nil
-}
-
 // SubmissionDetailsRequest is modified to use BooleanYN for fields requiring "yes" or "no" representation.
 type SubmissionDetailsRequest struct {
 	SID                         string `json:"sid"`
@@ -77,11 +41,11 @@ type SubmissionBasic struct {
 	Public                      BooleanYN `json:"public"`
 	MimeType                    string    `json:"mimetype"`
 	LatestMimeType              string    `json:"latest_mimetype"`
-	PageCount                   int       `json:"pagecount"`
-	RatingID                    int       `json:"rating_id"`
+	PageCount                   IntString `json:"pagecount"`
+	RatingID                    IntString `json:"rating_id"`
 	RatingName                  string    `json:"rating_name"`
 	ThumbnailDimensions
-	SubmissionTypeID int       `json:"submission_type_id"`
+	SubmissionTypeID IntString `json:"submission_type_id"`
 	TypeName         string    `json:"type_name"`
 	Digitalsales     BooleanYN `json:"digitalsales"`
 	Printsales       BooleanYN `json:"printsales"`
@@ -96,10 +60,10 @@ type Submission struct {
 		KeywordID   string    `json:"keyword_id"`
 		KeywordName string    `json:"keyword_name"`
 		Suggested   BooleanYN `json:"contributed"`
-		Count       int       `json:"submissions_count"`
+		Count       IntString `json:"submissions_count"`
 	} `json:"keywords"`
 	Favorite         BooleanYN `json:"favorite"`
-	FavoritesCount   int       `json:"favorites_count"`
+	FavoritesCount   IntString `json:"favorites_count"`
 	UserIconFileName string    `json:"user_icon_file_name"`
 	UserIconURL      struct {
 		Large  string `json:"user_icon_url_large,omitempty"`
@@ -115,13 +79,13 @@ type Submission struct {
 		MimeType              string    `json:"mimetype"`
 		SubmissionID          string    `json:"submission_id"`
 		UserID                string    `json:"user_id"`
-		SubmissionFileOrder   int       `json:"submission_file_order"`
-		SizeX                 int       `json:"full_size_x"`
-		SizeY                 int       `json:"full_size_y"`
-		ThumbX                int       `json:"thumb_huge_x,omitempty"`
-		ThumbY                int       `json:"thumb_huge_y,omitempty"`
-		ThumbNonCustomX       int       `json:"thumb_huge_noncustom_x,omitempty"`
-		ThumbNonCustomY       int       `json:"thumb_huge_noncustom_y,omitempty"`
+		SubmissionFileOrder   IntString `json:"submission_file_order"`
+		FullSizeX             IntString `json:"full_size_x"`
+		FullSizeY             IntString `json:"full_size_y"`
+		ThumbHugeX            IntString `json:"thumb_huge_x,omitempty"`
+		ThumbHugeY            IntString `json:"thumb_huge_y,omitempty"`
+		ThumbNonCustomX       IntString `json:"thumb_huge_noncustom_x,omitempty"`
+		ThumbNonCustomY       IntString `json:"thumb_huge_noncustom_y,omitempty"`
 		InitialFileMD5        string    `json:"initial_file_md5"`
 		FullFileMD5           string    `json:"full_file_md5"`
 		LargeFileMD5          string    `json:"large_file_md5"`
@@ -132,81 +96,81 @@ type Submission struct {
 		CreateDateTimeUser    string    `json:"create_datetime_usertime"`
 	} `json:"files"`
 	Pools []struct {
-		PoolID                     string `json:"pool_id"`
-		Name                       string `json:"name"`
-		Description                string `json:"description"`
-		Count                      int    `json:"count"`
-		LeftSubmissionID           string `json:"submission_left_submission_id"`
-		RightSubmissionID          string `json:"submission_right_submission_id"`
-		LeftSubmissionFileName     string `json:"submission_left_file_name"`
-		RightSubmissionFileName    string `json:"submission_right_file_name"`
-		LeftThumbnailURL           string `json:"submission_left_thumbnail_url,omitempty"`
-		RightThumbnailURL          string `json:"submission_right_thumbnail_url,omitempty"`
-		LeftThumbnailURLNonCustom  string `json:"submission_left_thumbnail_url_noncustom,omitempty"`
-		RightThumbnailURLNonCustom string `json:"submission_right_thumbnail_url_noncustom,omitempty"`
-		LeftThumbX                 int    `json:"submission_left_thumb_huge_x,omitempty"`
-		LeftThumbY                 int    `json:"submission_left_thumb_huge_y,omitempty"`
-		RightThumbX                int    `json:"submission_right_thumb_huge_x,omitempty"`
-		RightThumbY                int    `json:"submission_right_thumb_huge_y,omitempty"`
-		LeftThumbNonCustomX        int    `json:"submission_left_thumb_huge_noncustom_x,omitempty"`
-		LeftThumbNonCustomY        int    `json:"submission_left_thumb_huge_noncustom_y,omitempty"`
-		RightThumbNonCustomX       int    `json:"submission_right_thumb_huge_noncustom_x,omitempty"`
-		RightThumbNonCustomY       int    `json:"submission_right_thumb_huge_noncustom_y,omitempty"`
+		PoolID                     string    `json:"pool_id"`
+		Name                       string    `json:"name"`
+		Description                string    `json:"description"`
+		Count                      IntString `json:"count"`
+		LeftSubmissionID           string    `json:"submission_left_submission_id"`
+		RightSubmissionID          string    `json:"submission_right_submission_id"`
+		LeftSubmissionFileName     string    `json:"submission_left_file_name"`
+		RightSubmissionFileName    string    `json:"submission_right_file_name"`
+		LeftThumbnailURL           string    `json:"submission_left_thumbnail_url,omitempty"`
+		RightThumbnailURL          string    `json:"submission_right_thumbnail_url,omitempty"`
+		LeftThumbnailURLNonCustom  string    `json:"submission_left_thumbnail_url_noncustom,omitempty"`
+		RightThumbnailURLNonCustom string    `json:"submission_right_thumbnail_url_noncustom,omitempty"`
+		LeftThumbX                 IntString `json:"submission_left_thumb_huge_x,omitempty"`
+		LeftThumbY                 IntString `json:"submission_left_thumb_huge_y,omitempty"`
+		RightThumbX                IntString `json:"submission_right_thumb_huge_x,omitempty"`
+		RightThumbY                IntString `json:"submission_right_thumb_huge_y,omitempty"`
+		LeftThumbNonCustomX        IntString `json:"submission_left_thumb_huge_noncustom_x,omitempty"`
+		LeftThumbNonCustomY        IntString `json:"submission_left_thumb_huge_noncustom_y,omitempty"`
+		RightThumbNonCustomX       IntString `json:"submission_right_thumb_huge_noncustom_x,omitempty"`
+		RightThumbNonCustomY       IntString `json:"submission_right_thumb_huge_noncustom_y,omitempty"`
 	} `json:"pools"`
-	Description             string `json:"description"`
-	DescriptionBBCodeParsed string `json:"description_bbcode_parsed"`
-	Writing                 string `json:"writing"`
-	WritingBBCodeParsed     string `json:"writing_bbcode_parsed"`
-	PoolsCount              int    `json:"pools_count"`
+	Description             string    `json:"description"`
+	DescriptionBBCodeParsed string    `json:"description_bbcode_parsed"`
+	Writing                 string    `json:"writing"`
+	WritingBBCodeParsed     string    `json:"writing_bbcode_parsed"`
+	PoolsCount              IntString `json:"pools_count"`
 	Ratings                 []struct {
-		ContentTagID int    `json:"content_tag_id"`
-		Name         string `json:"name"`
-		Description  string `json:"description"`
-		RatingID     int    `json:"rating_id"`
+		ContentTagID IntString `json:"content_tag_id"`
+		Name         string    `json:"name"`
+		Description  string    `json:"description"`
+		RatingID     IntString `json:"rating_id"`
 	} `json:"ratings"`
-	CommentsCount    int       `json:"comments_count"`
-	Views            int       `json:"views"`
+	CommentsCount    IntString `json:"comments_count"`
+	Views            IntString `json:"views"`
 	SalesDescription string    `json:"sales_description"`
 	ForSale          BooleanYN `json:"forsale"`
-	DigitalPrice     int       `json:"digital_price"`
+	DigitalPrice     IntString `json:"digital_price"`
 	Prints           []struct {
-		PrintSizeID        int    `json:"print_size_id"`
-		Name               string `json:"name"`
-		Price              int    `json:"price"`
-		PriceOwnerDiscount int    `json:"price_owner_discount,omitempty"`
+		PrintSizeID        IntString   `json:"print_size_id"`
+		Name               string      `json:"name"`
+		Price              PriceString `json:"price"`
+		PriceOwnerDiscount PriceString `json:"price_owner_discount,omitempty"`
 	} `json:"prints"`
 }
 
 type ThumbnailDimensions struct {
-	ThumbMediumX                int `json:"thumb_medium_x,omitempty"`
-	ThumbLargeX                 int `json:"thumb_large_x,omitempty"`
-	ThumbHugeX                  int `json:"thumb_huge_x,omitempty"`
-	ThumbMediumY                int `json:"thumb_medium_y,omitempty"`
-	ThumbLargeY                 int `json:"thumb_large_y,omitempty"`
-	ThumbHugeY                  int `json:"thumb_huge_y,omitempty"`
-	ThumbMediumNonCustomX       int `json:"thumb_medium_noncustom_x,omitempty"`
-	ThumbLargeNonCustomX        int `json:"thumb_large_noncustom_x,omitempty"`
-	ThumbHugeNonCustomX         int `json:"thumb_huge_noncustom_x,omitempty"`
-	ThumbMediumNonCustomY       int `json:"thumb_medium_noncustom_y,omitempty"`
-	ThumbLargeNonCustomY        int `json:"thumb_large_noncustom_y,omitempty"`
-	ThumbHugeNonCustomY         int `json:"thumb_huge_noncustom_y,omitempty"`
-	LatestThumbMediumX          int `json:"latest_thumb_medium_x,omitempty"`
-	LatestThumbLargeX           int `json:"latest_thumb_large_x,omitempty"`
-	LatestThumbHugeX            int `json:"latest_thumb_huge_x,omitempty"`
-	LatestThumbMediumY          int `json:"latest_thumb_medium_y,omitempty"`
-	LatestThumbLargeY           int `json:"latest_thumb_large_y,omitempty"`
-	LatestThumbHugeY            int `json:"latest_thumb_huge_y,omitempty"`
-	LatestThumbMediumNonCustomX int `json:"latest_thumb_medium_noncustom_x,omitempty"`
-	LatestThumbLargeNonCustomX  int `json:"latest_thumb_large_noncustom_x,omitempty"`
-	LatestThumbHugeNonCustomX   int `json:"latest_thumb_huge_noncustom_x,omitempty"`
-	LatestThumbMediumNonCustomY int `json:"latest_thumb_medium_noncustom_y,omitempty"`
-	LatestThumbLargeNonCustomY  int `json:"latest_thumb_large_noncustom_y,omitempty"`
-	LatestThumbHugeNonCustomY   int `json:"latest_thumb_huge_noncustom_y,omitempty"`
+	ThumbMediumX                IntString `json:"thumb_medium_x,omitempty"`
+	ThumbLargeX                 IntString `json:"thumb_large_x,omitempty"`
+	ThumbHugeX                  IntString `json:"thumb_huge_x,omitempty"`
+	ThumbMediumY                IntString `json:"thumb_medium_y,omitempty"`
+	ThumbLargeY                 IntString `json:"thumb_large_y,omitempty"`
+	ThumbHugeY                  IntString `json:"thumb_huge_y,omitempty"`
+	ThumbMediumNonCustomX       IntString `json:"thumb_medium_noncustom_x,omitempty"`
+	ThumbLargeNonCustomX        IntString `json:"thumb_large_noncustom_x,omitempty"`
+	ThumbHugeNonCustomX         IntString `json:"thumb_huge_noncustom_x,omitempty"`
+	ThumbMediumNonCustomY       IntString `json:"thumb_medium_noncustom_y,omitempty"`
+	ThumbLargeNonCustomY        IntString `json:"thumb_large_noncustom_y,omitempty"`
+	ThumbHugeNonCustomY         IntString `json:"thumb_huge_noncustom_y,omitempty"`
+	LatestThumbMediumX          IntString `json:"latest_thumb_medium_x,omitempty"`
+	LatestThumbLargeX           IntString `json:"latest_thumb_large_x,omitempty"`
+	LatestThumbHugeX            IntString `json:"latest_thumb_huge_x,omitempty"`
+	LatestThumbMediumY          IntString `json:"latest_thumb_medium_y,omitempty"`
+	LatestThumbLargeY           IntString `json:"latest_thumb_large_y,omitempty"`
+	LatestThumbHugeY            IntString `json:"latest_thumb_huge_y,omitempty"`
+	LatestThumbMediumNonCustomX IntString `json:"latest_thumb_medium_noncustom_x,omitempty"`
+	LatestThumbLargeNonCustomX  IntString `json:"latest_thumb_large_noncustom_x,omitempty"`
+	LatestThumbHugeNonCustomX   IntString `json:"latest_thumb_huge_noncustom_x,omitempty"`
+	LatestThumbMediumNonCustomY IntString `json:"latest_thumb_medium_noncustom_y,omitempty"`
+	LatestThumbLargeNonCustomY  IntString `json:"latest_thumb_large_noncustom_y,omitempty"`
+	LatestThumbHugeNonCustomY   IntString `json:"latest_thumb_huge_noncustom_y,omitempty"`
 }
 
 type SubmissionDetailsResponse struct {
 	Sid          string       `json:"sid"`
-	ResultsCount int          `json:"results_count"`
+	ResultsCount IntString    `json:"results_count"`
 	UserLocation string       `json:"user_location"`
 	Submissions  []Submission `json:"submissions"`
 }
