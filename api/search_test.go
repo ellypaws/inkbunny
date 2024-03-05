ebc7b38ea01114249519a5b3b60bec819a662f2d
Elly <145195760+ellypaws@users.noreply.github.com>
2024-03-06 00:46:02 +0800

    Write random search in search_test.go

--- api/search_test.go
+++ api/search_test.go
@@ -42,3 +42,73 @@ func TestCredentials_SearchSubmissions(t *testing.T) {
 		t.Errorf("Expected submission id to be 14576, got %s", searchResponse.Submissions[0].SubmissionID)
 	}
 }
+
+func TestCredentials_SearchSubmissionsRandom(t *testing.T) {
+	user, err := Guest().Login()
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+
+	err = user.ChangeRating(Ratings{
+		General:        true,
+		Nudity:         true,
+		MildViolence:   true,
+		Sexual:         true,
+		StrongViolence: true,
+	})
+
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+	t.Logf("Logged in as %s, sid: %s\n", user.Username, user.Sid)
+
+	t.Logf("Searching for submissions")
+	searchResponse, err := user.SearchSubmissions(SubmissionSearchRequest{
+		SubmissionIDsOnly:  true,
+		SubmissionsPerPage: 5,
+		Page:               1,
+		Text:               "inkbunny",
+		Type:               SubmissionTypePicturePinup,
+		OrderBy:            "views",
+		Random:             true,
+		Scraps:             "both",
+	})
+	if err != nil {
+		t.Errorf("Error searching submissions: %v", err)
+	}
+
+	if len(searchResponse.Submissions) == 0 {
+		t.Fatal("No submissions found")
+	}
+
+	var submissionIDs string
+	const maxSubmissions = 5
+	for i := 0; i < min(maxSubmissions, len(searchResponse.Submissions)); i++ {
+		submissionIDs += searchResponse.Submissions[i].SubmissionID
+		if i != min(maxSubmissions-1, len(searchResponse.Submissions)-1) {
+			submissionIDs += ","
+		}
+	}
+
+	if submissionIDs == "" {
+		t.Fatal("No submission IDs found")
+	}
+
+	t.Logf("Getting submission details for IDs: %s", submissionIDs)
+	details, err := user.SubmissionDetails(
+		SubmissionDetailsRequest{
+			SubmissionIDs:   submissionIDs,
+			ShowDescription: Yes,
+		})
+	if err != nil {
+		t.Fatalf("Error getting submission details: %v", err)
+	}
+
+	if len(details.Submissions) == 0 {
+		t.Fatalf("Expected at least one submission, got none")
+	}
+
+	if details.Submissions[0].Title == "" {
+		t.Errorf("Expected title to be non-empty, got empty")
+	}
+}
